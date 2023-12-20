4d878e5daa0ce2af158b37646c720332e1a803bc
Bruno <101842175+Nexterzin@users.noreply.github.com>
2023-12-19 21:20:56 -0300

    Update login.page.dart
    New update in login page

--- login.page.dart
+++ login.page.dart
@@ -9,76 +9,106 @@ class LoginRep extends StatefulWidget {
 }
 
 class _LoginRepState extends State<LoginRep> {
-String login = '';
-String senha = '';
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String login = '';
+  String senha = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: const Key('loginScreen'),
       backgroundColor: Colors.deepPurple,
       body: Padding(
-          padding: EdgeInsets.all(5),
-          child: Center(
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: Form(
+            key: _formKey,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextFormField(
                   onChanged: (text) {
-                    login = text;
+                    setState(() {
+                      login = text;
+                    });
                   },
                   autofocus: true,
-                  style: new TextStyle(
+                  style: const TextStyle(
                     color: Colors.black,
-                    fontSize: 30
+                    fontSize: 20,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: "Login",
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
                     ),
-                  decoration: InputDecoration(
-                      labelText: "Login",
-                      labelStyle: TextStyle(
-                        color: Colors.white
-                        )
-                        ),
+                  ),
                 ),
-                SizedBox(height: 30),
+                const SizedBox(height: 30),
                 TextFormField(
                   onChanged: (text) {
-                    senha = text;
+                    setState(() {
+                      senha = text;
+                    });
                   },
                   autofocus: true,
                   obscureText: true,
-                  style: new TextStyle(
-                    color: Colors.black, 
-                    fontSize: 30
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: "Senha",
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
                     ),
-                  decoration: InputDecoration(
-                      labelText: "Senha",
-                      labelStyle: TextStyle(color: Colors.white)),
+                  ),
                 ),
-                SizedBox(height: 30),
+                const SizedBox(height: 30),
                 ButtonTheme(
-                    minWidth: 180.0,
-                    height: 60.0,
-                    child: RaisedButton(
-                      onPressed: () {
+                  minWidth: 220.0,
+                  height: 60.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState?.validate() ?? false) {
                         if (login == 'Bruno' && senha == '123') {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(builder: (context) => HomeApp()) 
+                          // ignore: avoid_print
+                          print('Login correto. Navegando para HomeApp.');
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const HomeApp(
+                                    login: 'Bruno', senha: '123')),
                           );
                         } else {
-                          print('login invalido');
+                          // ignore: avoid_print
+                          print('Login inválido');
                         }
-                      },
-                      child: Text(
-                        "ENTRAR",
-                        style: TextStyle(color: Colors.black),
+                      }
+                    },
+                    child: const Text(
+                      "ENTRAR",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
                       ),
-                      color: Colors.white,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(220.0, 60.0),
+                      backgroundColor: Colors.white,
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
+                        borderRadius: BorderRadius.circular(20.0),
                       ),
-                  ))
+                    ),
+                  ),
+                ),
               ],
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
