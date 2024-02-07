3948a6d2af4ab634eca30bfa2ab05a99a52d133a
Bruno <101842175+Nexterzin@users.noreply.github.com>
2024-02-06 21:58:30 -0300

    Update ListaClienteCABUKEM.dart
    Atualizações!
    
    Erro: 
    - Ainda não consegui salvar os clientes na lista

--- ListaClienteCABUKEM.dart
+++ ListaClienteCABUKEM.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ListaClienteCABUKEM extends StatelessWidget {
-  final List<String> listaClientes;
+class ListaClienteCABUKEM extends StatefulWidget {
+  const ListaClienteCABUKEM({Key? key}) : super(key: key);
 
-  ListaClienteCABUKEM({Key? key, required this.listaClientes}) : super(key: key);
+  @override
+  _ListaClienteCABUKEMState createState() => _ListaClienteCABUKEMState();
+}
+
+class _ListaClienteCABUKEMState extends State<ListaClienteCABUKEM> {
+  List<String> ListaClientesCABUKEM = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Carregar os clientes CABUKEM ao iniciar a tela
+    _carregarClientesCABUKEM();
+  }
+
+  // Função para carregar os clientes CABUKEM do SharedPreferences
+  Future<void> _carregarClientesCABUKEM() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      ListaClientesCABUKEM = prefs.getStringList('ListaClientesCABUKEM') ?? [];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,13 +42,13 @@ class ListaClienteCABUKEM extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-          itemCount: listaClientes.length,
+          itemCount: ListaClientesCABUKEM.length,
           itemBuilder: (context, index) {
             return Card(
               elevation: 5.0,
               margin: const EdgeInsets.symmetric(vertical: 8.0),
               child: ListTile(
-                title: Text(listaClientes[index]),
+                title: Text(ListaClientesCABUKEM[index]),
               ),
             );
           },
